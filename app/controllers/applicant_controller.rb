6dbde8490cb3103273037198911e72353319a9b7
Cheryl K <cherylkelson@icloud.com>
2019-04-25 20:39:07 +0000

    Delete current_user helper in ApplicantController

--- app/controllers/applicant_controller.rb
+++ app/controllers/applicant_controller.rb
@@ -1,17 +1,16 @@
-require './confi/environment'
+require './config/environment'
 class ApplicantController < Application Controller
 
 
   helpers do
 
     def logged_in?
-
+      !!current_user
     end
 
+  
 
-    def current_user
-
-    end
+end
 
 
 
