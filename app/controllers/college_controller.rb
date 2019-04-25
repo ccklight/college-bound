3a317d3c046918e0d502adb5a6870c33fe8206e9
Cheryl K <cherylkelson@icloud.com>
2019-04-25 20:57:11 +0000

    Add RESTful verbs to ApplicantController to be filled in with code later

---
+++ app/controllers/college_controller.rb
@@ -0,0 +1,36 @@
+require './config/environment'
+class CollegeController < ApplicationController
+
+  get '/' do
+    erb :welcome
+  end
+
+  get '/college'/new'
+
+  end
+
+  post '/college'
+
+  end
+
+  get '/college/:id'
+
+  end
+
+  get '/college/:id/edit'
+
+  end
+
+   patch
+
+  end
+
+  put '/college/:id'
+
+  end
+
+  delete '/college/:id'
+
+  end
+
+end
