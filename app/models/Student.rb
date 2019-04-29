0040add833c4bdf1205384372a09489418179e7f
Cheryl K <cherylkelson@icloud.com>
2019-04-29 16:18:34 +0000

    Correct file name from applicant to students in db/migrate/create_students

--- app/models/Student.rb
+++ app/models/Student.rb
@@ -1,3 +1,15 @@
 class Student < ActiveRecord::Base
-  has_many :colleges
+    has_many :colleges
+
+  has_secure_password
+
+  def slug
+    username.downcase.gsub("","-")
+    # name.downcase.gsub("","-")
+  end
+
+  def self.find_by_slug(slug)
+    Student.all.find{|student| student.slug == slug}
+  end
+
 end
