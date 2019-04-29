83228774f81ede0132941a4756edcf00683b55d7
Cheryl K <cherylkelson@icloud.com>
2019-04-29 16:19:53 +0000

    Comment out attributes of gpa, academic and athletic interests, hobbies, and awards from table for db/migrate/create_students for ease

--- db/migrate/create_students.rb
+++ db/migrate/create_students.rb
@@ -4,11 +4,11 @@ class CreateStudents < ActiveRecord::Base
       t.string :name
       t.string :email
       t.password :password_digest
-      t.integer :gpa
-      t.string :academic_interest
-      t.string :athletic_interest
-      t.string :hobbies
-      t.string :awards
+      # t.integer :gpa
+      # t.string :academic_interest
+      # t.string :athletic_interest
+      # t.string :hobbies
+      # t.string :awards
 
       t.timestamps null: false
     end
