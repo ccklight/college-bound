08d66d6e9731e12df409b8a4011e7a8822053e74
Cheryl K <cherylkelson@icloud.com>
2019-04-30 14:16:55 +0000

    Place get '/login' and post '/login before get '/registration' and post '/registration' to try to correctly route and render in StudentController

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,38 +1,16 @@
 require './config/environment'
 class StudentController < ApplicationController
 
-
-  get '/registration' do
-    if !logged_in?
-      erb :'/student/registration'
-    else
-      redirect '/colleges/layout'
-    end
-  end
-
-
-  post '/registration' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect 'student/registration'
-    else
-      @student = Student.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @student.save
-      session[:student_id] = @student.id
-      redirect  '/colleges'
-      end
-  end
-
-
-
   get '/login' do
     if logged_in?
-      erb :'show'
+      erb :'colleges/layout'
 
     else
       redirect 'student/registration'
 
     end
   end
+
 
   post '/login' do
     applicant = Applicant.find_by(:username => params[:username])
@@ -43,6 +21,34 @@ class StudentController < ApplicationController
       redirect 'student/registration'
     end
   end
+
+
+
+
+  get '/registration' do
+    if !logged_in?
+      erb :'/student/registration'
+    else
+      redirect '/student/login'
+    end
+  end
+
+
+  post '/registration' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect 'student/registration'
+    else
+      @student = Student.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      # @student = Student.new(name: params['name'], email: params['email'], password: params['password']
+      @student.save
+      session[:student_id] = @student_id
+      redirect  '/colleges/layout'
+      end
+  end
+
+
+
+
 
 
 
