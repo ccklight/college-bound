c303fee2dc0e0c06ed6309f35b5f8c5a60f8a6ad
Cheryl K <cherylkelson@icloud.com>
2019-04-30 14:23:42 +0000

    Place get and post login after get and post registration routes in StudentController to try to fix route and render problem

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,29 +1,6 @@
 require './config/environment'
 class StudentController < ApplicationController
 
-  get '/login' do
-    if logged_in?
-      erb :'colleges/layout'
-
-    else
-      redirect 'student/registration'
-
-    end
-  end
-
-
-  post '/login' do
-    applicant = Applicant.find_by(:username => params[:username])
-    if applicant && user.authenticate(params[:password])
-      session[:applicant_id] = applicant.id
-      redirect '/colleges'
-    else
-      redirect 'student/registration'
-    end
-  end
-
-
-
 
   get '/registration' do
     if !logged_in?
@@ -44,6 +21,28 @@ class StudentController < ApplicationController
       session[:student_id] = @student_id
       redirect  '/colleges/layout'
       end
+  end
+
+  get '/login' do
+    if logged_in?
+      erb :'colleges/layout'
+
+    else
+      "Hello World"
+      redirect 'student/registration'
+
+    end
+  end
+
+
+  post '/login' do
+    applicant = Applicant.find_by(:username => params[:username])
+    if applicant && user.authenticate(params[:password])
+      session[:applicant_id] = applicant.id
+      redirect '/colleges'
+    else
+      redirect 'student/registration'
+    end
   end
 
 
