0d77cff2165914ce9958b5987b866878103bc0b1
Cheryl K <cherylkelson@icloud.com>
2019-05-01 15:28:04 +0000

    Change adapter and database words to try to fix non-display of Students table upon running Shotgun

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,14 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security" 
+    set :session_secret, "password_security"
   end
 
   get '/' do
     erb :index
   end
 
-
+  
   helpers do
     def logged_in?
       !!current_student
--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,17 @@
 require_relative '../config/environment'
 
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
+  # :adapter => 'sqlite3_adapter.rb',
+  :adapter => 'sqlite3',
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  # :database =>  'sinatra_application.sqlite3.db'
+  # :database => "db/#{ENV['SINATRA_ENV']}.sqlite3"
 )
 
 require './app/controllers/application_controller'
