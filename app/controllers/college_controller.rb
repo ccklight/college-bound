82414435cdf41df3214b6b5f4287b1c8ec72724c
Cheryl K <cherylkelson@icloud.com>
2019-05-30 15:38:48 +0000

    Add previously completed lab solution from Fwitter Lab to compare code for patch and delete in collegecontroller.rb

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -46,7 +46,7 @@ class CollegeController < ApplicationController
   get '/college/:id/edit' do
     if logged_in?
       @college = College.find_by_id(params[:id])
-    if  @college || @college.student == current_student
+    if  @college && @college.student == current_student
       erb :'/college/edit'
     else
       redirect '/college/new'
@@ -57,7 +57,7 @@ class CollegeController < ApplicationController
 
 
     patch '/college/:id' do
-        @college.student == current_user
+         @college.student == current_user
       if @college && @college.student == current_student
          @college = College.find_by_id(params[:id])
          @college.name = params[:name]
@@ -69,7 +69,29 @@ class CollegeController < ApplicationController
 
     end
   end
-
+***********
+FWITTER
+patch '/tweets/:id' do
+    if logged_in?
+      if params[:content] == ""
+          redirect "/tweets/#{params[:id]}/edit"
+    else
+        @tweet = Tweet.find_by_id(params[:id])
+    if @tweet && @tweet.user == current_user
+        if @tweet.update(content: params[:content])
+          redirect "/tweets/#{@tweet.id}"
+    else
+          redirect "/tweets/#{@tweet.id}/edit"
+          end
+    else
+          redirect '/tweets'
+        end
+    end
+    else
+          redirect '/login'
+        end
+    end
+***********
 
 
     delete '/college/:id' do
@@ -80,4 +102,21 @@ class CollegeController < ApplicationController
     redirect '/college'
     end
   end
+************
+FWITTER
+delete '/tweets/:id/delete' do
+      if logged_in?
+        @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        @tweet.delete
+      end
+
+        redirect '/tweets'
+      else
+        redirect '/login'
+      end
+    end
+
+*************
+
 end
